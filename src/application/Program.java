545825ccabd28a1141bf74d225b50d1aa1a25c23
Cristiano Medeiros <cdsmedeiros@gmail.com>
2022-11-29 20:18:16 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,8 @@
-package aplication;
+package application;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,4 +1,4 @@
-package aplication;
+package application;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
